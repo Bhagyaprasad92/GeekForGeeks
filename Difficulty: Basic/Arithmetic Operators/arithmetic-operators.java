272e611f5d531ee9510e1b0a8bf420aa94b5ef44
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-14 22:35:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        // code here
+        int p = x + y, q = x - y, r = x * y, s = x / y, t = x % y;
+
+        // The below code prints the output
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
