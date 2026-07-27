73dd43eca83c242586b5dd9b466010161aeebfb7
BHAGYA PRASAD <147487694+Bhagyaprasad92@users.noreply.github.com>
2026-07-27 21:19:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Decision Making/decision-making.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        if(n < m) System.out.println("less");
+        if(n > m) System.out.println("greater");
+        if(n == m) System.out.println("equal");
+    }
+}
